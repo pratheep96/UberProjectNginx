0f215cf8f2688065b827b38b173dbaa744d93b89
Pratheep Premaraj <PPremaraj@spartaglobal.com>
2019-08-16 14:41:54 +0100

    integration testing successful

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -13,17 +13,25 @@ describe 'nginx_uber::default' do
     platform 'ubuntu', '18.04'
 
     it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+      expect {chef_run}.to_not raise_error
     end
-  end
-
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should install nginx' do
+      expect(chef_run).to install_package 'nginx'
+    end
+    it 'should enable nginx' do
+      expect(chef_run).to enable_service 'nginx'
+    end
+    it 'should run nginx' do
+      expect(chef_run).to start_service 'nginx'
+    end
+    it 'should a create a proxy.conf template' do
+      expect(chef_run).to create_template('/etc/nginx/sites-availables/proxy.conf').with_variables(proxy_port: 3000)
+    end
+    it 'should a create symlink' do
+      expect(chef_run).to create_link('/etc/nginx/sites-enabled/proxy.conf').with_link_type(:symbolic)
+    end
+    it 'should delete the symlink' do
+      expect(chef_run).to delete_link('/etc/nginx/sites-enabled/default')
     end
   end
 end
